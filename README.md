83f0c5aeb70fabdfbc2f3b3e738e53a402095a27
eladk333 <eladkatz100@gmail.com>
2025-05-31 23:11:38 +0300

    React done

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Targilon9
+
+https://github.com/eladk333/Targilon9
+
+Elad Katz
+Id: 322587064
---
+++ week9-react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Week 9</h1>
+      <Hello name="Elad" />
+      <Hello name="Neta" />
+    </div>
+  );
+}
+
+export default App;
